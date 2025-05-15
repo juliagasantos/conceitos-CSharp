95137edd19926fc997622889f5bc6417a5347b67
juliagasantos <juliagalvaoalmeidasantos@gmail.com>
2025-05-14 22:17:47 -0300

    inicializacao

---
+++ Conceitos-CSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿//tipos reaiscw 
+byte b = 10;
+int idade = 25;
+long populacao = 8000000000000;
+
+//tipos reais
+float peso = 60.2f;
+double altura = 1.78;
+decimal salario = 2500.99m;
+
+
+//lógicos e texto
+bool aprovado = true;
+char inicial = 'J';
+string nome = "Júlia";
+
+//inferência com var
+var cidade = "São Paulo";
+var cidadeT = 'S';
+var nota = 8.5;
+var letra = 'A';
+var boleana = true;
+
+Console.WriteLine("Nome: "+ nome);
+Console.WriteLine($"Nome: {nome}");
+Console.WriteLine($"Idade: {idade}");
